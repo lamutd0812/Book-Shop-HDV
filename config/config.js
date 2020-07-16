f30eb52a627144f07c7fdcb5804abd4d79e7b35c
Lam Nguyen <46679494+lamutd0812@users.noreply.github.com>
2020-07-16 09:34:59 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    'db_uri': 'mongodb+srv://lamnguyen:4jic1WzLSNouEhSv@cluster0-rkh3t.mongodb.net/bookstore?retryWrites=true&w=majority',
+    'db_uri': '',
     'facebook_auth': {
-        'appId': '599901297333131',
-        'appSecret': 'bc1a4772426a61c1340543239d5a9796',
+        'appId': '',
+        'appSecret': '',
         'callbackUrl': 'http://localhost:3000/auth/facebook/callback'
     },
     'google_auth': {
-        'clientId': '328126663048-t9rktm6gbkktv118kvkt17eec9crfl6v.apps.googleusercontent.com',
-        'clientSecret': 'IR2TsYCIlAdiw7vycYT5atdX',
+        'clientId': '',
+        'clientSecret': '',
         'callbackUrl': 'http://localhost:3000/auth/google/callback'
     }
 };
