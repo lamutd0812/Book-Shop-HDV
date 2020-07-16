ca616c7d84ef154ed97675936108dbccb17e10b8
Lam Nguyen <46679494+lamutd0812@users.noreply.github.com>
2020-07-16 09:35:29 +0700

    Update paypal.js

--- config/paypal.js
+++ config/paypal.js
@@ -3,8 +3,8 @@ const paypal = require('paypal-rest-sdk');
 // paypal config
 paypal.configure({
     'mode': 'sandbox',
-    'client_id': 'Aaar_nI_ofKlWMSCqI3Wz9gGsiKltvV2O0erqvEpP9TTtc5D_ERPI0ioJrg5iPGFTRT5XhP5pC3bkJbd',
-    'client_secret': 'ED19aLGtMX1LeWHEJbgcxbX03fY9T_ol4FJDJAcGm-vej1vOszA6QXAiv0n2K3svp8UWdWc5jhjvWakx'
+    'client_id': '',
+    'client_secret': ''
 });
 
 module.exports = paypal;
